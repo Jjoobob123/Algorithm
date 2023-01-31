88cda66f297c829971e94145dc3a90ab732a25cd
Jjoobob123 <120294031+Jjoobob123@users.noreply.github.com>
2023-01-31 10:42:26 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -1,22 +1,11 @@
 function solution(s) {
-    let answer = "";
     s = s.toLowerCase()
-    // mutable => 원본 데이터를 변환시킨다는 뜻.
-    // immutable => 원본 데이터가 변환되지 않는다.
-    //대부분 통하는 경우 배열은 mutable 문자열은 immutable
-    let idx = 0
-    for(let i = 0; i < s.length; i++){
-        let word = s[i];
-        if(s[i] ===" "){
-            idx = 0
-        }else{
-            if(idx === 0){
-                word = s[i].toUpperCase()
-            }
-            idx ++;
-        }
-        answer += word
-        console.log(answer)
-    }
-    return answer
+         .split(" ")
+         .map((str) => {
+        return str === "" 
+        ? str 
+        : str[0].toUpperCase() + str.slice(1)
+    })
+    
+    return s.join(" ")
 }
