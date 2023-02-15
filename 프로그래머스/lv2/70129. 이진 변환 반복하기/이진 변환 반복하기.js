cbf2359547965ad8977aa6e00729b8e377cb4edb
Jjoobob123 <120294031+Jjoobob123@users.noreply.github.com>
2023-02-15 10:52:45 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 11.96 ms, Memory: 39.4 MB -BaekjoonHub

--- 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
+++ 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
@@ -1,24 +1,15 @@
 function solution(s) {
-    let count = 0; // 변환 횟수
-    let remove = 0; // 제거된 0의 개수
-    
-    while(s !== '1'){
-        count++; //변환 횟수 === 반복 횟수 
-        // 1. 모든 0을 제거 
-        let temp = '';
-        for(let i = 0; i< s.length; i++){
-            if(s[i] === '0'){
-                // 참조해온 요소가 '0'인 경우를 찾는다.
-                remove++;
-            } else {
-                // 문자열 '1'만 들어온다 
-                temp += s[i];
-            }
-        }
-        s = temp.length; // 결과가 문자열 '1'이 아니라면 , 다시 반복
-        s = s.toString(2); // 결과가 '1' 이라면 반복은 종료 
+    // recursion 함수 (재귀함수)
+    function recursion(s, [count,remove]){
+       if( s === '1'){
+           return [count, remove];
+       }
+        count++
+        remove += s.split('').filter(el => el === '0').length
+        s = s.split('').filter(el => el === '1').length
+        s = s.toString( 2 )
         
+        return recursion(s,[count,remove])
     }
-    
-    return [ count , remove]
+    return recursion(s,[0,0]); // [count, remove]
 }
