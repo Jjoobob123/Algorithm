8879bc6b7c8124fd6717e6ccd991f98d7ace6146
Jjoobob123 <120294031+Jjoobob123@users.noreply.github.com>
2023-02-17 00:49:49 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.06 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.js
@@ -0,0 +1,9 @@
+function solution(my_string) {
+    let aaa = ""
+    let answer = my_string.toLowerCase() // 소문자로 변환
+                          .split('') // 배열로 변환
+                          .sort() // 오름차순으로 변환
+                          .join('')
+    return aaa = answer
+    
+}
