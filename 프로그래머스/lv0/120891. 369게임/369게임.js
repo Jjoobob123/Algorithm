2179573e781b7dc772bf22b78da5ca2febb18bc1
Jjoobob123 <120294031+Jjoobob123@users.noreply.github.com>
2023-03-01 14:47:47 +0900

    [level 0] Title: 369게임, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120891. 369게임/369게임.js
@@ -0,0 +1,13 @@
+function solution(order) {
+  let count = 0
+  
+  let str = String(order).split('')
+  
+  for(let i = 0; i < str.length; i ++){
+    if(str[i] === '3'||str[i] === '6'||str[i] === '9'){
+      count ++
+    }
+  }
+return count
+
+}
