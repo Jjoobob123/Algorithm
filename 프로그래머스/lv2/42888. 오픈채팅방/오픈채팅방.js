5ef3a78333eb4d4ea61066c3f990222c6da647ce
Jjoobob123 <120294031+Jjoobob123@users.noreply.github.com>
2023-03-03 10:23:55 +0900

    [level 2] Title: 오픈채팅방, Time: 119.94 ms, Memory: 82 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.js
@@ -0,0 +1,24 @@
+function solution(record) {
+    const answer = []
+    
+    const user = {} // 유저들의 최종 닉네입 값을 저장 
+    for( let i = 0; i < record.length; i++){
+       const [action, uid, nickname] = record[i].split(' ')
+       
+       if( nickname ){
+           user [ uid ] = nickname;
+           
+       }
+       if(action !== "Change"){
+           answer.push({action,uid})
+       }
+    }
+    for(let idx in answer){
+        answer[ idx ] = user[ answer[ idx ].uid ] + (
+            answer[ idx ].action === 'Enter'
+                ? "님이 들어왔습니다."
+                : "님이 나갔습니다."
+        )
+    }
+    return answer
+}
