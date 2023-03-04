c8185685808d450da11be0ecaf08a4948ca9f865
Jjoobob123 <120294031+Jjoobob123@users.noreply.github.com>
2023-03-04 16:38:20 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.js
@@ -0,0 +1,17 @@
+function solution(age) {
+  const space ={
+    0 : 'a',
+    1 : 'b',
+    2 : 'c',
+    3 : 'd',
+    4 : 'e',
+    5 : 'f',
+    6 : 'g',
+    7 : 'h',
+    8 : 'i',
+    9 : 'j'  
+  }
+  const aaa = String(age)
+  
+  return [...aaa].map(x => space[x]).join('')
+}
