7283a5aa267066d939a1e8b46911f2d474b2fbbd
Jjoobob123 <120294031+Jjoobob123@users.noreply.github.com>
2023-03-12 16:14:22 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
@@ -1,22 +1,15 @@
-function solution(n) {
-    // 제곱근
-    // 제곱의 기준이 되는 숫자
-    // 2 * 2 === 4, 2 는 4의 제곱근
-    let answer = -1;
-    
-    // i * i === n, i 는 n의 제곱근
-    for(let i = 1; i * i <= n; i++){
-        // 11 * 11 === 121 , 11은 121의 제곱근
-        // console.log(i, i*i)
-        if(i*i === n ){
-            // console.log(i,n)
-            // answer = i + 1;
-            // return answer * answer 
-            // return (i+1) * (i+1)
-            // 거듭제곱 연산자 활용도 가능
-            return (i + 1)**2
-        }
-    }
-    // 제곱근을 찾지 못한 경우
-    return answer
+function solution(n){
+  // 거듭제곱근 sprt 활용
+  let num = 0
+  let sqrt = Math.sqrt(n)
+  // console.log(sqrt)
+  if( !Number.isInteger(sqrt)) {return -1}
+  else{
+    num = sqrt + 1
+  }
+  
+  return Math.pow(num,2)
+  // console.log(num)
+
+  
 }
