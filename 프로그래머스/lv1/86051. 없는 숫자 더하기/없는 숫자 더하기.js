8872b2a1b92a40e9e0fbd80b17f1a95070a24888
Jjoobob123 <120294031+Jjoobob123@users.noreply.github.com>
2023-03-12 17:16:56 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 2.53 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -1,11 +1,15 @@
 function solution(numbers) {
-    let answer = 0;
-    
-    for(let  i = 1; i <= 9 ; i++){
-        if(numbers.includes(i) === false){
-            answer += i
-        }
-    }
-    console.log(answer)
+    const answer = new Array(9).fill(1)
+                    .reduce((acc,cur,i)=> {
+                        console.log(acc)
+                        const num = cur + i;
+                        return acc + (numbers.includes(num)
+                                     //배열에 존재한다면 0을 더해준다.
+                                      ? 0
+                                      // 배열에 존재하지 않는다면
+                                      : num
+                                     )
+                    },0)
     return answer
+    
 }
