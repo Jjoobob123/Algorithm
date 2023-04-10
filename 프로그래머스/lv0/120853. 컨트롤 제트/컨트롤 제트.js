24cac46fb9043027286984cd173c34c2eed06fcd
Jjoobob123 <120294031+Jjoobob123@users.noreply.github.com>
2023-04-10 12:57:36 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.16 ms, Memory: 32 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.js
@@ -0,0 +1,22 @@
+function solution(s) {
+    const arr = s.split(" ")
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] === 'Z'){
+            if(arr[i-1]){
+                arr.splice(i-1, 2);
+                i -= 2;
+              
+            }
+            else{
+                arr.splice(i,1)
+                i--; 
+            }
+        }
+    }
+  
+    return arr.reduce((a,b) => a*1 + b*1, 0);
+}
+
+
+
+solution("1 2 Z 3")
